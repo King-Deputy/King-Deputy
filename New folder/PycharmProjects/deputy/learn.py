f374ae05f5f866dfef992d9d8995f4350e4c7d88
Akogun Sodiq <datanation16@gmail.com>
2022-03-23 15:12:48 +0100

    Refreshed

--- New folder/PycharmProjects/deputy/learn.py
+++
@@ -1,8 +0,0 @@
-class too:
-    def __init__(self, measurement, uses):
-        self.measurement = measurement
-        self.uses = uses
-Chisquare = too("counting data", "test association")
-ttest = too("continuous data", "test difference")
-correlation = too("continuous data", "test linear relationship")
-
--- New folder/PycharmProjects/sodiq/myfile.py
+++
@@ -1,5 +0,0 @@
-x=6
-y=8
-z=x*y
-print(z)
-import numpy as np
