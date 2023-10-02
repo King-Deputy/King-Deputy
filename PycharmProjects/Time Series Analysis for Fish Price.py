e2fd8fba96e9997736f99fa8ffd1c6555b83224b
King-Deputy <datanation16@gmail.com>
2023-10-02 15:28:45 +0100

    Added fish code

--- PycharmProjects/Time Series Analysis for Fish Price.py
+++ PycharmProjects/Time Series Analysis for Fish Price.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
-plt.rcParams.update({'figure.figsize'=(10, 7), 'figure.dpi':120})
+plt.rcParams.update({'figure.figsize':(10, 7), 'figure.dpi':120})
 
 #Import as DataFrame
 df = pd.read_csv('C:/Users/USER/Desktop/DN BATCH 5/AL-IMAM/FISH_DATA.csv')
